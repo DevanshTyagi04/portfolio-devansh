38892c28eccd062b11f77b0397775e62dccb05e1
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-01-12 18:42:04 +0530

    Implement ShowCase Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Hero from "./sections/Hero"
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
         <Hero />
+        <ShowcaseSection />
     </main>
   )
 }
