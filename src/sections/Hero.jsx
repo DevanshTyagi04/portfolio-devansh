e761758097220f02ac83ed903c3ee0d20e33eeeb
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-01-12 17:46:20 +0530

    Complete Hero 3D Model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button"
+import HeroExperience from "../components/models/hero_models/HeroExperience"
 import { words } from "../constants"
 
 
@@ -46,6 +47,11 @@ const Hero = () => {
                 </div>
             </header>
             {/* RIGHT: 3D MODEL */}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience />
+                </div>
+            </figure>
         </div>
     </section>
   )
