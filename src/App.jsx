6c590c65f0bdd3935eb401670d49e354eead7782
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-01-12 19:15:13 +0530

    Implement Logo Showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import LogoSection from "./components/LogoSection";
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -12,6 +13,7 @@ const App = () => {
         <NavBar />
         <Hero />
         <ShowcaseSection />
+        <LogoSection />
     </main>
   )
 }
