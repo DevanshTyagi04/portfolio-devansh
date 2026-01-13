c3cd46023854cf423aef8f7c28c0a1a6e861d9ae
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-01-13 18:18:18 +0530

    Implement Footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -24,6 +25,7 @@ const App = () => {
         <TechStack />
         <Testimonials />
         <Contact />
+        <Footer />
     </main>
   )
 }
