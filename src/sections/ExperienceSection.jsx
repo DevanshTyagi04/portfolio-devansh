c5e3d2e1d7470c5544748a1c99046bfce5c8c8d2
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-01-13 15:20:05 +0530

    Implement GlowCard Effect Component

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,4 +1,6 @@
+import GlowCard from "../components/GlowCard"
 import TitleHeader from "../components/TitleHeader"
+import { expCards } from "../constants"
 
 
 const ExperienceSection = () => {
@@ -6,6 +8,24 @@ const ExperienceSection = () => {
     <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">
         <div className="w-full h-full md:px-20 px-5">
             <TitleHeader title="Professional Work Experience" sub="💼 My Career Overview" />
+
+            <div className="mt-32 realtive">
+                <div className="mt-32 relative">
+                    <div className="relative z-50 xl:space-y-32 space-y-10">
+                        {expCards.map((card,index) => (
+                            <div key={card.title} className="exp-card-wrapper">
+                                <div className="xl:w-2/6">
+                                    <GlowCard card={card} index={index}>
+                                        <div>
+                                            <img src={card.imgPath} alt={card.title} />
+                                        </div>
+                                    </GlowCard>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
   )
